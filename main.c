0b394a4be7bb4b545a0bc6831afb7a09d451083f
7thFox <dev7thFox@gmail.com>
2024-01-26 22:07:21 -0600

    more complex number support

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "stdbool.h"
 #include <stdlib.h>
+#include <math.h>
 
 #define PREC_MIN -999
+#define MAX_INT_PARTS = 128;
 
 enum token_kind_t {
     // Special
@@ -24,11 +26,9 @@ enum token_kind_t {
 struct token_t {
     enum token_kind_t kind;
 
-    union {
-        long        val_long;
-        int         val_int;
-        const char *val_string;
-    };
+    bool                negative;
+    unsigned long long  integer_part;
+    long long           exp;
 };
 
 struct node_t {
@@ -48,8 +48,7 @@ struct node_t*  node_make();
 struct node_t*  parse_binaryexp(struct node_t *left, int minprec);
 struct node_t*  parse_expression(int minprec);
 struct node_t*  parse_leaf();
-void            statement_execute(struct node_t *node);
-long            statement_long_val(struct node_t *node);
+struct token_t  statement_execute(struct node_t *node);
 bool            token_is_binary(struct token_t token);
 int             token_get_prec(struct token_t token);
 struct token_t  token_next();
@@ -90,10 +89,88 @@ bool class_is_ws(int c) {
 void display_token(struct token_t token) {
     switch (token.kind) {
         case TOK_INVALID:
-            fprintf(stdout, "\x1b[35m??\x1b[0m");
+            fprintf(stdout, "\x1b[35mINVALID\x1b[0m");
             break;
         case TOK_LITERAL_INTEGER_NOSIZE:
-            fprintf(stdout, "\x1b[34m{NUM: %li}\x1b[0m", token.val_long);
+            fprintf(stdout, "\x1b[34m{NUM: \x1b[0m");
+            if (token.integer_part == 0)
+            {
+                putc('0', stdout);
+            }
+            else
+            {
+                if (token.negative)
+                {
+                    putc('-', stdout);
+                }
+
+                char buffer[100];
+                buffer[99] = '\0';
+                int i = 98;
+                long long exp = token.exp;
+                unsigned long long disp = token.integer_part;
+
+                if (token.exp < 0)
+                {
+                    for (; i >= 0 && disp > 0; i--)
+                    {
+                        if (exp == 0) {
+                            buffer[i] = '.';
+                            exp++;
+                            continue;
+                        }
+                        int x = disp % 10;
+                        buffer[i] = x + '0';
+                        disp /= 10;
+                        exp++;
+                    }
+                    for (; i >= 0 && exp < 0; i--)
+                    {
+                        buffer[i] = '0';
+                        exp++;
+                    }
+                    if (i == 0)
+                    {
+                        buffer[i--] = '.';
+                        buffer[i--] = '0';
+                    }
+                }
+                else
+                {
+                    int comma = 0;
+
+                    for (; i >= 0 && exp > 0; i--)
+                    {
+                        if (comma == 3)
+                        {
+                            buffer[i] = ',';
+                            comma = 0;
+                            continue;
+                        }
+
+                        buffer[i] = '0';
+                        exp--;
+                        comma++;
+                    }
+
+                    for (; i >= 0 && disp > 0; i--)
+                    {
+                        if (comma == 3)
+                        {
+                            buffer[i] = ',';
+                            comma = 0;
+                            continue;
+                        }
+
+                        int x = disp % 10;
+                        buffer[i] = x + '0';
+                        disp /= 10;
+                        comma++;
+                    }
+                }
+                fprintf(stdout, "%s", buffer+i+1);
+            }
+            fprintf(stdout, "\x1b[34m}\x1b[0m");
             break;
         default:
             putc(token.kind, stdout);
@@ -196,23 +273,114 @@ struct node_t* parse_leaf() {
     return node;
 }
 
-void statement_execute(struct node_t *node)
+struct token_t statement_execute(struct node_t *node)
 {
-    fprintf(stdout, "  = %li\n", statement_long_val(node));
-}
+    if (token_is_binary(node->token))
+    {
+        struct token_t left = statement_execute(node->left);
+        struct token_t right = statement_execute(node->right);
 
-long statement_long_val(struct node_t *node)
-{
+        if (left.kind != TOK_LITERAL_INTEGER_NOSIZE ||
+            right.kind != TOK_LITERAL_INTEGER_NOSIZE)
+        {
+            return (struct token_t){ .kind = TOK_INVALID };
+        }
+    
+        if (node->token.kind == TOK_MINUS)
+        {
+            right.negative = !right.negative;
+        }
+
+        switch (node->token.kind)
+        {
+            case TOK_PLUS:
+            case TOK_MINUS:
+
+                // 1e3 + 1e0
+                //           = 1000 + 1
+                //           = 1001 
+                //           = 1001e0
+
+
+                // 1e3 + 1e-2
+                //           = 1000 + 0.01
+                //           = 1000.01 
+                //           = 100001e-2
+
+                // 1e3 - 1e0
+                //           = 1000 - 1
+                //           =  999 
+                //           =  999e0
+
+
+                // 1e3 - 1e-2
+                //           = 1000 - 0.01
+                //           =  999.99 
+                //           =  99999e-2
+
+                // TODO JOSH: Safety
+                long long expmin, expmax;
+                if (left.exp > right.exp)
+                {
+                    expmin = right.exp;
+                    expmax = left.exp;
+
+                    for (int p = expmax - expmin; p > 0; p--)
+                    {
+                        left.integer_part *= 10;
+                    }
+                }
+                else
+                {
+                    expmin = left.exp;
+                    expmax = right.exp;
+
+                    for (int p = expmax - expmin; p > 0; p--)
+                    {
+                        right.integer_part *= 10;
+                    }
+                }
+
+                left.exp = expmin;
+                if (left.negative == right.negative)
+                {
+                    left.integer_part += right.integer_part;
+                }
+                else
+                {
+                    if (left.integer_part > right.integer_part)
+                    {
+                        left.integer_part -= right.integer_part;
+                    }
+                    else
+                    {
+                        left.integer_part = right.integer_part - left.integer_part;
+                    }
+                }
+                break;
+            // case TOK_MULTIPLY:
+            //     break;
+            // case TOK_DIVIDE:
+            //     break;
+
+            default: 
+                return (struct token_t){ .kind = TOK_INVALID };
+        }
+
+        return left;
+    }
+    
     switch (node->token.kind)
     {
-        case TOK_LITERAL_INTEGER_NOSIZE: return node->token.val_long;
+        case TOK_LITERAL_INTEGER_NOSIZE: return node->token;
 
-        case TOK_PLUS:     return statement_long_val(node->left) + statement_long_val(node->right);
-        case TOK_MINUS:    return statement_long_val(node->left) - statement_long_val(node->right);
-        case TOK_MULTIPLY: return statement_long_val(node->left) * statement_long_val(node->right);
-        case TOK_DIVIDE:   return statement_long_val(node->left) / statement_long_val(node->right);
+        // case TOK_PLUS:     return statement_long_val(node->left) + statement_long_val(node->right);
+        // case TOK_MINUS:    return statement_long_val(node->left) - statement_long_val(node->right);
+        // case TOK_MULTIPLY: return statement_long_val(node->left) * statement_long_val(node->right);
+        // case TOK_DIVIDE:   return statement_long_val(node->left) / statement_long_val(node->right);
 
-        default: return 0;
+        default: 
+            return (struct token_t){ .kind = TOK_INVALID };
     }
 }
 
@@ -269,20 +437,59 @@ struct token_t token_next() {
             negative = true;
             c = char_next();
         }
-        long val_long = c - '0';
+        long long exp = 0;
+        unsigned long long integer_part = c - '0';
 
         while (class_is_numeric(char_peek())) {
             c = char_next();
-            val_long *= 10;
-            val_long += c - '0';
+            integer_part *= 10;
+            integer_part += c - '0';
         }
 
-        if (negative) val_long *= -1;
+        if (char_peek() == '.') 
+        {
+            char_next();
+
+            while (class_is_numeric(char_peek())) {
+                c = char_next();
+                integer_part *= 10;
+                integer_part += c - '0';
+                exp--;
+            }
+        }
+
+        if (char_peek() == 'E' ||
+            char_peek() == 'e')
+        {
+            char_next();
+
+            bool exp_negative = false;
+            if (char_peek() == '-')
+            {
+                char_next();
+                exp_negative = true;
+            }
+
+            // TODO JOSH: Error if non-numeric
+
+            long long exp_given = 0;
+            while (class_is_numeric(char_peek())) {
+                c = char_next();
+                exp_given *= 10;
+                exp_given += c - '0';
+            }
+
+            if (exp_negative) exp_given *= -1;
+
+            exp += exp_given;
+        }
 
         return (struct token_t)
         {
-            .kind     = TOK_LITERAL_INTEGER_NOSIZE,
-            .val_long = val_long,
+            .kind           = TOK_LITERAL_INTEGER_NOSIZE,
+            .negative       = negative,
+            .integer_part   = integer_part,
+            .exp            = exp,
         };
     }
 
@@ -322,10 +529,13 @@ int main() {
     {
         fprintf(stdout, "> ");
         struct node_t *n = parse_expression(PREC_MIN);
-        display_node(n);
+        // display_node(n);
         // putc('\n', stdout);
 
-        statement_execute(n);
+        struct token_t result = statement_execute(n);
+        fprintf(stdout, "  = ");
+        display_token(result);
+        putc('\n', stdout);
 
         nodes_size = 0;
         _peek_char = 0;
